ae040c2b68ae3236299dbd66b94467ed0f251de1
Rohit Singh <errohitsingh086@gmail.com>
2021-09-07 17:23:33 +0530

    Updated Millipixels Assignment
    I Just Update the Assignment where i am adding the logical interface of dice.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React,{useState,useCallback} from 'react'
 import "./App.css"
+
+const diceImage = {
+  1:require("./images/1.png").default,
+  2:require("./images/2.png").default,
+  3:require("./images/3.png").default,
+  4:require("./images/4.png").default,
+  5:require("./images/5.png").default,
+  6:require("./images/6.png").default,
+}
+
 const App = () => {
 
   const [diceNumber, setDiceNumber] = useState(1)
@@ -7,13 +17,13 @@ const App = () => {
   const handleClick = useCallback((min,max) => {
     let diceValue = Math.floor(Math.random() * (max - min)) + min;
     setDiceNumber(diceValue)
-  }, []);
+  },[])
 
   return (
     <div className="container">
       <h1>Dice Game in ReactJS</h1>
       <div className="dice-block">
-        {diceNumber}
+        <img src={diceImage[diceNumber]} style={{width:"300px",height:"300px"}} alt={diceNumber}/>
       </div>
       <button className="btn-style" onClick={() => handleClick(1,7)}>Roll Button</button>
     </div>
